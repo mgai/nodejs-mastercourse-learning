db6ef05fac38a0d2fed5f7c189a8ac7fb86c18f8
GAI Ming <ming.gai@gmail.com>
2018-08-30 21:06:09 +0800

    Tokens.post and Tokens.get are ready.

--- rest-api-homework-assignment-2/lib/tokens.js
+++ rest-api-homework-assignment-2/lib/tokens.js
@@ -26,6 +26,46 @@ tokens.get = (data, callback) => {
 };
 
 /**
+ * tokens.post to create a token, also it implies user login.
+ * @param data - to provide {email, password}
+ * @param callback 
+ */
+tokens.post = (data, callback) => {
+    const email = helpers.validate(data.payload.email, { type: 'string'});
+    const password = helpers.validate(data.payload.password, { type: 'string'});
+    if(email && password) {
+        const userId = helpers.md5(email);
+        if(userId) {
+            _data.read('users', userId, (err, user) => {
+                if(!err && user) {
+                    const hashedPassword = helpers.hash(password);
+                    if(hashedPassword == user.hashedPassword) {
+                        const id = helpers.createRandomString(20);
+                        const expires = Date.now() + 1000 * 3600; // One hour.
+                        const token = {userId, expires};
+                        _data.create('tokens', id, token, function(err) {
+                            if(!err) {
+                                callback(200, {...token, id});
+                            } else {
+                                callback(500, {'Error': 'Failed to create token.'});
+                            }
+                        })
+                    } else {
+                        callback(404, {'Error': 'Invalid user ID or password.'});
+                    }
+                } else {
+                    callback(404, {'Error': 'User is not found'});
+                }
+            });
+        } else {
+            callback(500, {'Error': 'Failed to retrieve user id via email.'});
+        }
+    } else {
+        callback(400, {'Error': 'Missing required field(s).'});
+    }
+}
+
+/**
  * Check if the provided token is valid for the user.
  * @param {tokenId, userId}
  * @param callback (200 when valid, 400/404 otherwise.)
