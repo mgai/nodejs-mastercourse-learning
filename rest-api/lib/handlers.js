23498770fe7e0259c1a6e110464285c3688d2d1b
mgai <ming.gai@gmail.com>
2018-09-07 11:29:33 +0800

    DONE: Page 2: Create an Account.
    Note - I've copied all the app.js and app.css from FINAL github of the class.

--- rest-api/lib/handlers.js
+++ rest-api/lib/handlers.js
@@ -48,6 +48,40 @@ handlers.index = function(data, callback) {
 }
 
 /**
+ * Account Creation handler
+ */
+handlers.accountCreate = function(data, callback) {
+    // Handle only GET method.
+    if(data.method === 'get') {
+        // Prepare data for interpolation.
+        let templateData = {
+            'head.title': 'Create an Account',
+            'head.description': 'Signup is easy and only takes a few seconds.',
+            'body.class': 'accountCreate'
+        };
+        // Read in a template as a string.
+        helpers.getTemplate('accountCreate', templateData, function(err, str) {
+            if(!err && str) {
+                // Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function(err, str) {
+                    if(!err && str) {
+                        callback(200, str, 'html');
+                    } else {
+                        debug('Failed when addUniversalTemplates', err, str);
+                        callback(500, undefined, 'html');
+                    }
+                });
+            } else {
+                debug('Failed whem getTemplate', err, str);
+                callback(500, undefined, 'html');
+            }
+        });
+    } else {
+        callback(405, undefined, 'html');   // 405 - Not allowed.
+    }
+}
+
+/**
  * Favicon handler.
  * @param data the request object.
  * @param callback (code, data, type)
