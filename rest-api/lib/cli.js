aaae0cf8e757a485753e7228b99b161d75d7dfe6
mgai <ming.gai@gmail.com>
2018-09-10 17:04:54 +0800

    DONE Commands 3: Stats

--- rest-api/lib/cli.js
+++ rest-api/lib/cli.js
@@ -4,6 +4,8 @@
 'use strict';
 
 // Dependencies.
+const os = require('os');
+const v8 = require('v8');
 const readline = require('readline');
 const util = require('util');
 const debug = util.debuglog('cli');
@@ -141,7 +143,45 @@ cli.responders.exit = function() {
 };
 
 cli.responders.stats = function() {
-    console.log('You asked for stats.');
+    // Compile an object of stats.
+    const stats = {
+        'Load Average': os.loadavg().join(' '),
+        'CPU Count': os.cpus().length,
+        'Free Memory': os.freemem(),
+        'Current Malloced Memory': v8.getHeapStatistics().malloced_memory,
+        'Peak Malloced Memory': v8.getHeapStatistics().peak_malloced_memory,
+        'Allocated Heap Used (%)': Math.round(v8.getHeapStatistics().used_heap_size / v8.getHeapStatistics().total_heap_size * 100),
+        'Avalable Heap Allocated (%)': Math.round(v8.getHeapStatistics().total_heap_size / v8.getHeapStatistics().heap_size_limit * 100),
+        'Uptime': os.uptime() + ' seconds'
+    };
+
+    // Create a header for the stats.
+    cli.horizontalLine();
+    cli.centered('System Statistics');
+    cli.horizontalLine();
+    cli.verticalSpace(2);
+
+    // Log out each stat.
+    for(let key in stats) {
+        if(stats.hasOwnProperty(key)) {
+            let value = stats[key];
+            let line = helpers.ansiColorString.YELLOW.replace('%s', key);
+ 
+            // Add padding to same width - 60 char.
+            let padding = 60 - line.length;
+            for (let i=0; i<padding; i++) {
+                line+=' ';
+            };
+            line += value;
+ 
+            console.log(line);
+            cli.verticalSpace(1);
+        }
+    }
+ 
+    cli.verticalSpace(1);
+    // End with another horizontal line.
+    cli.horizontalLine();
 };
 
 cli.responders.listUsers = function() {
